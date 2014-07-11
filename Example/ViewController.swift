bdf4e03be069f8af9d32f671d40f825251e6154b
Alex <alexander.orlov@loxal.net>
2014-07-11 18:07:54 +0200

    configure an "on clicked" behavior for a button

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -1,25 +1,32 @@
-//
-//  ViewController.swift
-//  Example
-//
-//  Created by Alexander Orlov on 11.07.14.
-//  Copyright (c) 2014 loxal. All rights reserved.
-//
+//  Copyright 2014 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
 
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var button: UIButton
+    @IBOutlet var textField: UITextField
+    @IBOutlet var label: UILabel
+    
+    func configureButton() {
+        button.addTarget(self, action: "onButtonClicked:", forControlEvents: .TouchUpInside)
+    }
+    
+    func onButtonClicked(sender: UIButton) {
+        NSLog("Button clicked")
+        label.text = "Hello, " + textField.text + "!"
+        textField.text = textField.text + " happened!"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        println("Do any additional setup after loading the view, typically from a nib.")
+        configureButton()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        println("Dispose of any resources that can be recreated.")
     }
-
-
 }
 
